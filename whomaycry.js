53b009fb3f76bb6a9080994e400ba3acb3861aa6
Aiden <144732086+i-love-nana@users.noreply.github.com>
2023-11-30 14:19:34 -0700

    Update whomaycry.js

--- whomaycry.js
+++ whomaycry.js
@@ -4,7 +4,7 @@ nameOfElement = "demo";
 // English code
 alert("Howdy! this is a stange place...");
 console.log("I hope you enjoy learning Javascript!");
-
+const person = {firstName:"Aiden", lastName:"Daniels", age:31, favoriteColor:"purple"};
 
 function changeText(){
   document.getElementById("demo").innerHTML= "This is a bucket"
