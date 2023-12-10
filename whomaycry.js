90e3d7cb26ec2dae48a562f3153014aee304b48c
Aiden <144732086+i-love-nana@users.noreply.github.com>
2023-12-10 13:04:34 -0700

    Update whomaycry.js

--- whomaycry.js
+++ whomaycry.js
@@ -1,11 +1,15 @@
 let nameOfElement;
 nameOfElement = "demo";
+nameOfElement = "demo2";
 
 // English code
 alert("Howdy! this is a stange place isn't it?");
 console.log("I hope you enjoy learning Javascript!");
 function changeText(){
   document.getElementById("demo").innerHTML= "ooo what silly secret you've found here, reload the page to reset it"
+}
+function changeText(){
+document.getElementById("demo2").innerHTML= "Yet another silly pointless secret"
 }
 let person = {
   firstName: "Aiden",
