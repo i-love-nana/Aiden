0d1c061f55e7ae5e0dfa7786e267fedd0479b2f6
Aiden <144732086+i-love-nana@users.noreply.github.com>
2023-12-11 13:45:33 -0700

    Update whomaycry.js

--- whomaycry.js
+++ whomaycry.js
@@ -1,6 +1,5 @@
 let nameOfElement;
 nameOfElement = "demo";
-nameOfElement = "demo2";
 
 // English code
 alert("Howdy! this is a stange place isn't it?");
@@ -8,9 +7,7 @@ console.log("I hope you enjoy learning Javascript!");
 function changeText(){
   document.getElementById("demo").innerHTML= "ooo what silly secret you've found here, reload the page to reset it"
 }
-function changeText(){
-document.getElementById("demo2").innerHTML= "Yet another silly pointless secret"
-}
+
 let person = {
   firstName: "Aiden",
   lastName: "Daniels",
