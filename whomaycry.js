c89df458c2b3296cc9bf03ae6d65b45d37845b2d
Aiden <144732086+i-love-nana@users.noreply.github.com>
2023-12-11 13:41:45 -0700

    Update whomaycry.js

--- whomaycry.js
+++ whomaycry.js
@@ -28,3 +28,15 @@ person.Age ++;
 window.alert(person.Age);
   
 }
+function Days(){
+// To display the final no. of days (result)
+  window.alert ("it's been" + Difference_In_days + "days");
+}
+
+let date1 = new Date("9/11/23");
+let date2 = new date();
+// To calculate the time difference of two dates
+let Difference_in_time = date2.getTime() - date1.gettime();
+// to calculate the no. of days between two dates
+let difference_in_time =
+  Math.round(difference_in_time / (1000 * 3600 * 24));
